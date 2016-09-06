e24eee1225dbd6de5008b094929537d8f2bfefd6
Manish Reddy <manish@Manishs-MacBook-Pro.local>
2016-09-05 17:10:39 -0700

    Added extra Doc

--- ButtonAndLabelActivitySpinner/ButtonActivitySpinner.swift
+++ ButtonAndLabelActivitySpinner/ButtonActivitySpinner.swift
@@ -9,6 +9,7 @@
 import Foundation
 import UIKit
 
+///Button that can display activity spinner in place of text to indicate that request is being processed.
 @IBDesignable public class ButtonActivitySpinner: UIButton {
     
     var text: String = "" {
@@ -19,6 +20,9 @@ import UIKit
         }
     }
     
+    /**
+     Activity indicator which is default UIActivity Indicator.
+     */
     public var activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray) {
         didSet {
             setNeedsLayout()
@@ -35,23 +39,32 @@ import UIKit
         initialiseView()
     }
     
-    
+    ///Initialises the View
     private func initialiseView() {
         addSubview(activityIndicator)
         setNeedsLayout()
         
     }
     
+    ///Layout subviews which takes care of activityindicator and label frames.
     public override func layoutSubviews() {
         super.layoutSubviews()
         activityIndicator.frame = bounds
     }
     
+    /**
+     startAnimating will set the text to nil,
+     start animating the activity indicator.
+     */
     public func startAnimating() {
         setTitle("", forState: UIControlState.Normal)
         activityIndicator.startAnimating()
     }
     
+    /**
+     stopAnimating will set back the original text,
+     stop animating the activity indicator.
+     */
     public func stopAnimating() {
         setTitle(text, forState: UIControlState.Normal)
         activityIndicator.stopAnimating()
